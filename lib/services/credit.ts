64bda7f0fc9d2016526a3068f6315fdf5675464e
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2017-01-16 23:06:03 +0900

    SASAKI_TICKET-58 use request-promise-native[gmo-service].

--- lib/services/credit.ts
+++ lib/services/credit.ts
@@ -1,4 +1,4 @@
-import request = require("request");
+import request = require("request-promise-native");
 import querystring = require("querystring");
 
 /**
@@ -18,32 +18,27 @@ export namespace entryTranInterface {
         access_pass: string,
     }
 
-    export function call(args: Args) {
-        return new Promise((resolve: (result: Result) => void, reject: (err: Error) => void) => {
-            console.log("requesting...");
-            request.post({
-                url: `${process.env.GMO_ENDPOINT}/payment/EntryTran.idPass`,
-                form: {
-                    ShopID: args.shop_id,
-                    ShopPass: args.shop_pass,
-                    OrderID: args.order_id,
-                    JobCd: args.job_cd,
-                    Amount: args.amount,
-                }
-            }, (error, response, body) => {
-                console.log("request processed.", error, body);
-                if (error) return reject(error);
-                if (response.statusCode !== 200) return reject(new Error(body));
-
-                let result = querystring.parse(body);
-                if (result.ErrCode) return reject(new Error(body));
-
-                resolve({
-                    access_id: result.AccessID,
-                    access_pass: result.AccessPass,
-                });
-            });
+    export async function call(args: Args): Promise<Result> {
+        console.log("requesting...", args);
+        let body = await request.post({
+            url: `${process.env.GMO_ENDPOINT}/payment/EntryTran.idPass`,
+            form: {
+                ShopID: args.shop_id,
+                ShopPass: args.shop_pass,
+                OrderID: args.order_id,
+                JobCd: args.job_cd,
+                Amount: args.amount,
+            }
         });
+        console.log("request processed.", body);
+
+        let result = querystring.parse(body);
+        if (result.ErrCode) throw new Error(body);
+
+        return {
+            access_id: result.AccessID,
+            access_pass: result.AccessPass,
+        };
     }
 }
 
@@ -82,50 +77,45 @@ export namespace execTranInterface {
         client_field3: string,
     }
 
-    export function call(args: Args) {
-        return new Promise((resolve: (result: Result) => void, reject: (err: Error) => void) => {
-            console.log("requesting...", args);
-            request.post({
-                url: `${process.env.GMO_ENDPOINT}/payment/ExecTran.idPass`,
-                form: {
-                    AccessID: args.access_id,
-                    AccessPass: args.access_pass,
-                    OrderID: args.order_id,
-                    Method: args.method,
-                    PayTimes: args.pay_times,
-                    CardNo: args.card_no,
-                    Expire: args.expire,
-                    SecurityCode: args.security_code,
-                    Token: args.token,
-                    PIN: args.pin,
-                    ClientField1: args.client_field1,
-                    ClientField2: args.client_field2,
-                    ClientField3: args.client_field3,
-                }
-            }, (error, response, body) => {
-                console.log("request processed.", error, body);
-                if (error) return reject(error);
-                if (response.statusCode !== 200) return reject(new Error(body));
-
-                let result = querystring.parse(body);
-                if (result.ErrCode) return reject(new Error(body));
-
-                resolve({
-                    acs: result.ACS,
-                    order_id: result.OrderID,
-                    forward: result.Forward,
-                    method: result.Method,
-                    pay_times: result.PayTimes,
-                    approve: result.Approve,
-                    tran_id: result.TranID,
-                    tran_date: result.TranDate,
-                    check_string: result.CheckString,
-                    client_field1: result.ClientField1,
-                    client_field2: result.ClientField2,
-                    client_field3: result.ClientField3,
-                });
-            });
+    export async function call(args: Args): Promise<Result> {
+        console.log("requesting...", args);
+        let body = await request.post({
+            url: `${process.env.GMO_ENDPOINT}/payment/ExecTran.idPass`,
+            form: {
+                AccessID: args.access_id,
+                AccessPass: args.access_pass,
+                OrderID: args.order_id,
+                Method: args.method,
+                PayTimes: args.pay_times,
+                CardNo: args.card_no,
+                Expire: args.expire,
+                SecurityCode: args.security_code,
+                Token: args.token,
+                PIN: args.pin,
+                ClientField1: args.client_field1,
+                ClientField2: args.client_field2,
+                ClientField3: args.client_field3,
+            }
         });
+        console.log("request processed.", body);
+
+        let result = querystring.parse(body);
+        if (result.ErrCode) throw new Error(body);
+
+        return {
+            acs: result.ACS,
+            order_id: result.OrderID,
+            forward: result.Forward,
+            method: result.Method,
+            pay_times: result.PayTimes,
+            approve: result.Approve,
+            tran_id: result.TranID,
+            tran_date: result.TranDate,
+            check_string: result.CheckString,
+            client_field1: result.ClientField1,
+            client_field2: result.ClientField2,
+            client_field3: result.ClientField3,
+        };
     }
 }
 
@@ -152,37 +142,32 @@ export namespace alterTranInterface {
         tran_date: string,
     }
 
-    export function call(args: Args) {
-        return new Promise((resolve: (result: Result) => void, reject: (err: Error) => void) => {
-            console.log("requesting...", args);
-            request.post({
-                url: `${process.env.GMO_ENDPOINT}/payment/AlterTran.idPass`,
-                form: {
-                    ShopID: args.shop_id,
-                    ShopPass: args.shop_pass,
-                    AccessID: args.access_id,
-                    AccessPass: args.access_pass,
-                    JobCd: args.job_cd,
-                    Amount: args.amount,
-                    Method: args.method,
-                }
-            }, (error, response, body) => {
-                console.log("request processed.", error, body);
-                if (error) return reject(error);
-                if (response.statusCode !== 200) return reject(new Error(body));
-
-                let result = querystring.parse(body);
-                if (result.ErrCode) return reject(new Error(body));
-
-                resolve({
-                    access_id: result.AccessID,
-                    access_pass: result.AccessPass,
-                    forward: result.Forward,
-                    approve: result.Approve,
-                    tran_id: result.TranID,
-                    tran_date: result.TranDate,
-                });
-            });
+    export async function call(args: Args): Promise<Result> {
+        console.log("requesting...", args);
+        let body = await request.post({
+            url: `${process.env.GMO_ENDPOINT}/payment/AlterTran.idPass`,
+            form: {
+                ShopID: args.shop_id,
+                ShopPass: args.shop_pass,
+                AccessID: args.access_id,
+                AccessPass: args.access_pass,
+                JobCd: args.job_cd,
+                Amount: args.amount,
+                Method: args.method,
+            }
         });
+        console.log("request processed.", body);
+
+        let result = querystring.parse(body);
+        if (result.ErrCode) throw new Error(body);
+
+        return {
+            access_id: result.AccessID,
+            access_pass: result.AccessPass,
+            forward: result.Forward,
+            approve: result.Approve,
+            tran_id: result.TranID,
+            tran_date: result.TranDate,
+        };
     }
 }
