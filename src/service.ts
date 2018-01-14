46814ab7689f37e36966625f5e8353c11a9da774
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-01-14 09:31:38 +0000

    # GMO-13 ### Added - リクエストハンドラーをカスタマイズできるサービスクラスを追加。

--- src/service.ts
+++ src/service.ts
@@ -1,4 +1,5 @@
-import * as request from 'request-promise-native';
+import * as qs from 'qs';
+// import * as request from 'request-promise-native';
 
 import { DefaultTransporter, Transporter } from './transporters';
 
@@ -20,6 +21,17 @@ export interface IOptions {
     transporter?: Transporter;
 }
 
+export interface IFetchOptions {
+    uri: string;
+    form?: any;
+    qs?: any;
+    method: string;
+    headers?: {
+        [key: string]: any;
+    };
+    body?: any;
+}
+
 /**
  * base service class
  * @export
@@ -27,45 +39,97 @@ export interface IOptions {
  */
 export class Service {
     public options: IOptions;
-    public requestOptions: request.RequestPromiseOptions;
 
-    constructor(options: IOptions, requestOptions?: request.RequestPromiseOptions) {
+    constructor(options: IOptions) {
         this.options = options;
-
-        this.requestOptions = {
-            headers: {},
-            method: 'GET',
-            resolveWithFullResponse: true,
-            forever: true
-        };
-        if (requestOptions !== undefined) {
-            this.requestOptions = { ...this.requestOptions, ...requestOptions };
-        }
     }
 
     /**
      * Create and send request to API
      */
-    public async request(options: request.OptionsWithUri) {
-        options = { ...this.requestOptions, ...options };
+    public async request(options: IFetchOptions) {
+        const defaultOptions = {
+            headers: {},
+            method: 'GET'
+        };
+        options = { ...defaultOptions, ...options };
 
         const baseUrl = this.options.endpoint;
-        const url = `${baseUrl}${options.uri}`;
+        let url = `${baseUrl}${options.uri}`;
 
-        delete options.uri;
+        const querystrings = qs.stringify(options.qs);
+        url += (querystrings.length > 0) ? `?${querystrings}` : '';
 
-        // const querystrings = qs.stringify(options.qs);
-        // url += (querystrings.length > 0) ? `?${querystrings}` : '';
+        const headers = {
+            ...{
+                Accept: 'application/json',
+                'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
+                // 'Content-Type': 'application/json'
+            },
+            ...options.headers
+        };
 
-        const requestOptions = {
-            url: url,
-            ...options
+        const fetchOptions = {
+            method: options.method,
+            headers: headers,
+            // body: JSON.stringify(options.body),
+            body: qs.stringify(options.form)
         };
 
         // create request (using authClient or otherwise and return request obj)
         const transporter =
             (this.options.transporter !== undefined) ? this.options.transporter : new DefaultTransporter();
 
-        return transporter.request(requestOptions);
+        return transporter.request(url, fetchOptions);
     }
 }
+
+/**
+ * base service class
+ * @export
+ * @class Service
+ */
+// export class Service {
+//     public options: IOptions;
+//     public requestOptions: request.RequestPromiseOptions;
+
+//     constructor(options: IOptions, requestOptions?: request.RequestPromiseOptions) {
+//         this.options = options;
+
+//         this.requestOptions = {
+//             headers: {},
+//             method: 'GET',
+//             resolveWithFullResponse: true,
+//             forever: true
+//         };
+//         if (requestOptions !== undefined) {
+//             this.requestOptions = { ...this.requestOptions, ...requestOptions };
+//         }
+//     }
+
+//     /**
+//      * Create and send request to API
+//      */
+//     public async request(options: request.OptionsWithUri) {
+//         options = { ...this.requestOptions, ...options };
+
+//         const baseUrl = this.options.endpoint;
+//         const url = `${baseUrl}${options.uri}`;
+
+//         delete options.uri;
+
+//         // const querystrings = qs.stringify(options.qs);
+//         // url += (querystrings.length > 0) ? `?${querystrings}` : '';
+
+//         const requestOptions = {
+//             url: url,
+//             ...options
+//         };
+
+//         // create request (using authClient or otherwise and return request obj)
+//         const transporter =
+//             (this.options.transporter !== undefined) ? this.options.transporter : new DefaultTransporter();
+
+//         return transporter.request(requestOptions);
+//     }
+// }
--- src/transporters.ts
+++ src/transporters.ts
@@ -6,7 +6,8 @@
  */
 
 import * as createDebug from 'debug';
-import * as request from 'request-promise-native';
+// import * as request from 'request-promise-native';
+import * as fetch from 'isomorphic-fetch';
 
 const debug = createDebug('gmo-service:transporters');
 // tslint:disable-next-line
@@ -20,8 +21,11 @@ const pkg = require('../package.json');
  * @abstract
  */
 export abstract class Transporter {
-    public abstract async request(options: request.OptionsWithUrl): Promise<any>;
+    public abstract async request(url: string, options: RequestInit): Promise<any>;
 }
+// export abstract class Transporter {
+//     public abstract async request(options: request.OptionsWithUrl): Promise<any>;
+// }
 
 export type IBodyResponseCallback = Promise<any>;
 
@@ -47,12 +51,31 @@ export class StubTransporter implements Transporter {
         this.body = body;
     }
 
-    public async request(options: request.OptionsWithUrl) {
-        debug('requesting...', options);
+    public async request(url: string, options: RequestInit) {
+        debug('requesting...', url, options);
 
         return this.body;
     }
 }
+
+/**
+ * stub transporter
+ * スタブトランポーター
+ * @export
+ * @class
+ */
+// export class StubTransporter implements Transporter {
+//     public body: any;
+//     constructor(body: any) {
+//         this.body = body;
+//     }
+
+//     public async request(options: request.OptionsWithUrl) {
+//         debug('requesting...', options);
+
+//         return this.body;
+//     }
+// }
 
 /**
  * DefaultTransporter
@@ -65,19 +88,18 @@ export class DefaultTransporter implements Transporter {
      */
     public static readonly USER_AGENT: string = `gmo-service-nodejs-client/${pkg.version}`;
 
-    // constructor() {
-    // }
-
     /**
      * Configures request options before making a request.
      */
-    public static CONFIGURE(options: request.OptionsWithUrl): request.OptionsWithUrl {
+    public static CONFIGURE(options: RequestInit): RequestInit {
         // set transporter user agent
         options.headers = (options.headers !== undefined) ? options.headers : {};
-        if (!options.headers['User-Agent']) {
-            options.headers['User-Agent'] = DefaultTransporter.USER_AGENT;
-        } else if (options.headers['User-Agent'].indexOf(DefaultTransporter.USER_AGENT) === -1) {
-            options.headers['User-Agent'] = `${options.headers['User-Agent']} ${DefaultTransporter.USER_AGENT}`;
+        // tslint:disable-next-line:no-single-line-block-comment
+        /* istanbul ignore else */
+        if (!(<any>options.headers)['User-Agent']) {
+            (<any>options.headers)['User-Agent'] = DefaultTransporter.USER_AGENT;
+        } else if ((<any>options.headers)['User-Agent'].indexOf(DefaultTransporter.USER_AGENT) === -1) {
+            (<any>options.headers)['User-Agent'] = `${(<any>options.headers)['User-Agent']} ${DefaultTransporter.USER_AGENT}`;
         }
 
         return options;
@@ -86,47 +108,134 @@ export class DefaultTransporter implements Transporter {
     /**
      * Makes a request with given options and invokes callback.
      */
-    public async request(options: request.OptionsWithUrl) {
-        const requestOptions = DefaultTransporter.CONFIGURE(options);
+    public async request(url: string, options: RequestInit) {
+        const fetchOptions = DefaultTransporter.CONFIGURE(options);
 
-        return request(requestOptions).then((res) => this.wrapCallback(res));
+        debug('fetching...', url, fetchOptions);
+
+        return fetch(url, fetchOptions).then(async (response) => this.wrapCallback(response));
     }
 
     /**
      * Wraps the response callback.
      */
     // tslint:disable-next-line:prefer-function-over-method
-    private wrapCallback(res: request.FullResponse): any {
+    private async wrapCallback(response: Response): Promise<any> {
         const err: RequestError = new RequestError('An unexpected error occurred');
 
-        debug('request processed', res.statusCode, res.body);
-        if (res.statusCode !== undefined) {
-            return res.body;
-
-            // if (this.expectedStatusCodes.indexOf(res.statusCode) < 0) {
-            //     if (typeof res.body === 'string') {
-            //         // Consider all 4xx and 5xx responses errors.
-            //         err = new RequestError(res.body);
-            //         err.code = res.statusCode;
-            //     }
-
-            //     if (typeof res.body === 'object' && res.body.errors !== undefined) {
-            //         // consider 400
-            //         err = new RequestError((<any[]>res.body.errors).map((error) => `${error.title}:${error.detail}`).join('\n'));
-            //         err.code = res.statusCode;
-            //         err.errors = res.body.errors;
-            //     }
-            // } else {
-            //     if (res.body !== undefined) {
-            //         // consider 200,201,404
-            //         return res.body;
-            //     } else {
-            //         // consider 204
-            //         return;
-            //     }
-            // }
+        debug('request processed', response.status);
+        if (Number.isInteger(response.status)) {
+            return response.clone().text();
         }
+
+        // if (this.expectedStatusCodes.indexOf(response.status) < 0) {
+        //     // Consider all 4xx and 5xx responses errors.
+        //     let body: any;
+        //     try {
+        //         // Only and only application/json responses should
+        //         // be decoded back to JSON, but there are cases API back-ends
+        //         // responds without proper content-type.
+        //         body = await response.clone().json();
+        //     } catch (error) {
+        //         body = await response.clone().text();
+        //     }
+
+        //     if (typeof body === 'object' && body.error !== undefined) {
+        //         err = new RequestError(body.error.message);
+        //         err.code = response.status;
+        //         err.errors = body.error.errors;
+        //     } else {
+        //         err = new RequestError(body);
+        //         err.code = response.status;
+        //         err.errors = [];
+        //     }
+        // } else {
+        //     if (response.status === NO_CONTENT) {
+        //         // consider 204
+        //         return;
+        //     } else {
+        //         // consider 200,201
+        //         return response.json();
+        //     }
+        // }
 
         throw err;
     }
 }
+
+/**
+ * DefaultTransporter
+ * @export
+ * @class
+ */
+// export class DefaultTransporter implements Transporter {
+//     /**
+//      * Default user agent.
+//      */
+//     public static readonly USER_AGENT: string = `gmo-service-nodejs-client/${pkg.version}`;
+
+//     // constructor() {
+//     // }
+
+//     /**
+//      * Configures request options before making a request.
+//      */
+//     public static CONFIGURE(options: request.OptionsWithUrl): request.OptionsWithUrl {
+//         // set transporter user agent
+//         options.headers = (options.headers !== undefined) ? options.headers : {};
+//         if (!options.headers['User-Agent']) {
+//             options.headers['User-Agent'] = DefaultTransporter.USER_AGENT;
+//         } else if (options.headers['User-Agent'].indexOf(DefaultTransporter.USER_AGENT) === -1) {
+//             options.headers['User-Agent'] = `${options.headers['User-Agent']} ${DefaultTransporter.USER_AGENT}`;
+//         }
+
+//         return options;
+//     }
+
+//     /**
+//      * Makes a request with given options and invokes callback.
+//      */
+//     public async request(options: request.OptionsWithUrl) {
+//         const requestOptions = DefaultTransporter.CONFIGURE(options);
+
+//         return request(requestOptions).then((res) => this.wrapCallback(res));
+//     }
+
+//     /**
+//      * Wraps the response callback.
+//      */
+//     // tslint:disable-next-line:prefer-function-over-method
+//     private wrapCallback(res: request.FullResponse): any {
+//         const err: RequestError = new RequestError('An unexpected error occurred');
+
+//         debug('request processed', res.statusCode, res.body);
+//         if (res.statusCode !== undefined) {
+//             return res.body;
+
+//             // if (this.expectedStatusCodes.indexOf(res.statusCode) < 0) {
+//             //     if (typeof res.body === 'string') {
+//             //         // Consider all 4xx and 5xx responses errors.
+//             //         err = new RequestError(res.body);
+//             //         err.code = res.statusCode;
+//             //     }
+
+//             //     if (typeof res.body === 'object' && res.body.errors !== undefined) {
+//             //         // consider 400
+//             //         err = new RequestError((<any[]>res.body.errors).map((error) => `${error.title}:${error.detail}`).join('\n'));
+//             //         err.code = res.statusCode;
+//             //         err.errors = res.body.errors;
+//             //     }
+//             // } else {
+//             //     if (res.body !== undefined) {
+//             //         // consider 200,201,404
+//             //         return res.body;
+//             //     } else {
+//             //         // consider 204
+//             //         return;
+//             //     }
+//             // }
+//         }
+
+//         throw err;
+//     }
+// }
