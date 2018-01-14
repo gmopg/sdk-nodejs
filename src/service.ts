437174594dd2e33112d784756cecb8661fe4ca62
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-01-14 13:22:45 +0000

    # GMO-13 ### Added - リクエストハンドラーをカスタマイズできるサービスクラスを追加。

--- src/service.ts
+++ src/service.ts
@@ -1,5 +1,5 @@
-import * as qs from 'qs';
-// import * as request from 'request-promise-native';
+// import * as qs from 'qs';
+import * as request from 'request-promise-native';
 
 import { DefaultTransporter, Transporter } from './transporters';
 
@@ -37,99 +37,98 @@ export interface IFetchOptions {
  * @export
  * @class Service
  */
-export class Service {
-    public options: IOptions;
-
-    constructor(options: IOptions) {
-        this.options = options;
-    }
-
-    /**
-     * Create and send request to API
-     */
-    public async request(options: IFetchOptions) {
-        const defaultOptions = {
-            headers: {},
-            method: 'GET'
-        };
-        options = { ...defaultOptions, ...options };
-
-        const baseUrl = this.options.endpoint;
-        let url = `${baseUrl}${options.uri}`;
-
-        const querystrings = qs.stringify(options.qs);
-        url += (querystrings.length > 0) ? `?${querystrings}` : '';
-
-        const headers = {
-            ...{
-                Accept: 'application/json',
-                'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
-                // 'Content-Type': 'application/json'
-            },
-            ...options.headers
-        };
-
-        const fetchOptions = {
-            method: options.method,
-            headers: headers,
-            // body: JSON.stringify(options.body),
-            body: qs.stringify(options.form)
-        };
-
-        // create request (using authClient or otherwise and return request obj)
-        const transporter =
-            (this.options.transporter !== undefined) ? this.options.transporter : new DefaultTransporter();
-
-        return transporter.request(url, fetchOptions);
-    }
-}
-
-/**
- * base service class
- * @export
- * @class Service
- */
 // export class Service {
 //     public options: IOptions;
-//     public requestOptions: request.RequestPromiseOptions;
 
-//     constructor(options: IOptions, requestOptions?: request.RequestPromiseOptions) {
+//     constructor(options: IOptions) {
 //         this.options = options;
-
-//         this.requestOptions = {
-//             headers: {},
-//             method: 'GET',
-//             resolveWithFullResponse: true,
-//             forever: true
-//         };
-//         if (requestOptions !== undefined) {
-//             this.requestOptions = { ...this.requestOptions, ...requestOptions };
-//         }
 //     }
 
 //     /**
 //      * Create and send request to API
 //      */
-//     public async request(options: request.OptionsWithUri) {
-//         options = { ...this.requestOptions, ...options };
+//     public async request(options: IFetchOptions) {
+//         const defaultOptions = {
+//             headers: {},
+//             method: 'GET'
+//         };
+//         options = { ...defaultOptions, ...options };
 
 //         const baseUrl = this.options.endpoint;
-//         const url = `${baseUrl}${options.uri}`;
+//         let url = `${baseUrl}${options.uri}`;
 
-//         delete options.uri;
+//         const querystrings = qs.stringify(options.qs);
+//         url += (querystrings.length > 0) ? `?${querystrings}` : '';
 
-//         // const querystrings = qs.stringify(options.qs);
-//         // url += (querystrings.length > 0) ? `?${querystrings}` : '';
+//         const headers = {
+//             ...{
+//                 Accept: 'application/json',
+//                 'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
+//                 // 'Content-Type': 'application/json'
+//             },
+//             ...options.headers
+//         };
 
-//         const requestOptions = {
-//             url: url,
-//             ...options
+//         const fetchOptions = {
+//             method: options.method,
+//             headers: headers,
+//             // body: JSON.stringify(options.body),
+//             body: qs.stringify(options.form)
 //         };
 
 //         // create request (using authClient or otherwise and return request obj)
 //         const transporter =
 //             (this.options.transporter !== undefined) ? this.options.transporter : new DefaultTransporter();
 
-//         return transporter.request(requestOptions);
+//         return transporter.request(url, fetchOptions);
 //     }
 // }
+
+/**
+ * base service class
+ * @export
+ * @class Service
+ */
+export class Service {
+    public options: IOptions;
+    public requestOptions: request.RequestPromiseOptions;
+
+    constructor(options: IOptions, requestOptions?: request.RequestPromiseOptions) {
+        this.options = options;
+
+        this.requestOptions = {
+            headers: {},
+            method: 'GET',
+            resolveWithFullResponse: true
+        };
+        if (requestOptions !== undefined) {
+            this.requestOptions = { ...this.requestOptions, ...requestOptions };
+        }
+    }
+
+    /**
+     * Create and send request to API
+     */
+    public async request(options: request.OptionsWithUri) {
+        options = { ...this.requestOptions, ...options };
+
+        const baseUrl = this.options.endpoint;
+        const url = `${baseUrl}${options.uri}`;
+
+        delete options.uri;
+
+        // const querystrings = qs.stringify(options.qs);
+        // url += (querystrings.length > 0) ? `?${querystrings}` : '';
+
+        const requestOptions = {
+            url: url,
+            ...options
+        };
+
+        // create request (using authClient or otherwise and return request obj)
+        const transporter =
+            (this.options.transporter !== undefined) ? this.options.transporter : new DefaultTransporter();
+
+        return transporter.request(requestOptions);
+    }
+}
